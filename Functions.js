8ee495397409d65c267ad2ea523794c9e91423db
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-08 12:05:50 +0530

    Functions in Java Script

---
+++ Functions.js
@@ -0,0 +1,8 @@
+// 🧠 What are Functions?
+// Functions are blocks of reusable logic.
+// Instead of repeating the same task again and again, wrap it in a function and reuse it with
+// different inputs.
+// Think of a function like a vending machine:
+// Input: you give money + item code
+// Output: it gives you the item
+// Logic: hidden inside
