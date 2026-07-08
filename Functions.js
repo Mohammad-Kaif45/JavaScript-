e8874e017ba35af2961d156bbba26b78bfb9a72f
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-08 12:20:20 +0530

    Functions in Java Script

--- Functions.js
+++ Functions.js
@@ -6,3 +6,42 @@
 // Input: you give money + item code
 // Output: it gives you the item
 // Logic: hidden inside
+
+// Function Declarations
+function greet() {
+ console.log("Welcome to Sheryians!");
+}
+greet(); // You define it once, then call it whenever needed.
+
+// � Parameters vs Arguments
+
+function greet(name) {
+ console.log("Hello " + name);
+}
+greet("Harsh");
+
+// name is a parameter
+// "Harsh" is the argument you pass
+
+// 🌀 Return Values
+function sum(a, b) {
+ return a + b;
+}
+let total = sum(5, 10); // total is 15
+
+// return sends back a result to wherever the function was called
+// After return , function exits
+
+
+// Functions stored in variables
+// Cannot be hoisted (you can’t call them before they’re defined)
+
+// Default + Rest + Spread
+function multiply(a = 1, b = 1) {
+ return a * b;
+}
+function sum(...nums) {
+ return nums.reduce((acc, val) => acc + val, 0);
+}
+let nums = [1, 2, 3];
+console.log(sum(...nums)); // Spread
