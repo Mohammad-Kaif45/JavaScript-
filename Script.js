de83d200ea310a4c65123c09880430799c4a46eb
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-12 22:45:22 +0530

    DOM Manupulation satrt

--- Script.js
+++ Script.js
@@ -1,3 +1,10 @@
-let h1 = document.querySelector("h1");
-h1.style.color = "white";
-h1.style.backgroundColor = "blue";
+// Task 1 : Select the heading of a page by ID and change its text to "Welcom to Sheriyans"
+// let h1 = document.querySelector("#h1");
+// h1.textContent = "Welcome to Sheriyans";
+// console.dir(h1);
+// Tasks
+// Task 2 : Select all <li> elements and print their text using a loop.
+let lis = document.querySelectorAll("li"); // 
+lis.forEach(function(val){
+    console.log(val.textContent);
+});
