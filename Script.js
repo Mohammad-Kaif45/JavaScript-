fd128681327f930890bcbed9370f55048b4cce85
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-13 13:43:44 +0530

    DOM Manupulation satrt

--- Script.js
+++ Script.js
@@ -4,7 +4,19 @@
 // console.dir(h1);
 // Tasks
 // Task 2 : Select all <li> elements and print their text using a loop.
-let lis = document.querySelectorAll("li"); // 
-lis.forEach(function(val){
-    console.log(val.textContent);
-});
+// let lis = document.querySelectorAll("li"); // 
+// lis.forEach(function(val){
+//     console.log(val.textContent);
+// });
+
+// let img = document.querySelector("img");
+// img.setAttribute("src","https://lh3.googleusercontent.com/gps-cs-s/APNQkAGmYE6clA17N-O09V1Ebptawxm-_peZWUGNHp9ca2eRoPJHBCVtRYkwHp3CNLHL1kawucVylGAPS_XBM5sqXSX8kOQsrgK_vHH_aCuhlygvX8iivCCYmh2DH52Y6_51r8KBj7Fm=s1360-w1360-h1020-rw");
+
+// let a = document.querySelector("a");
+// a.href = "https://www.sheryians.com";
+
+// let div = document.querySelector("div");
+// div.setAttribute("title","Some Info");
+
+let button = document.querySelector("button");
+button.removeAttribute("disabled");
