2dce3056bf1060b20b706c9e089a321a6609c506
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-16 22:29:27 +0530

    E-Commerce

--- Master JS & Become a real developer/Script.js
+++ Master JS & Become a real developer/Script.js
@@ -1,40 +1,7 @@
-// Task 1 : Select the heading of a page by ID and change its text to "Welcom to Sheriyans"
-// let h1 = document.querySelector("#h1");
-// h1.textContent = "Welcome to Sheriyans";
-// console.dir(h1);
-// Tasks
-// Task 2 : Select all <li> elements and print their text using a loop.
-// let lis = document.querySelectorAll("li"); // 
-// lis.forEach(function(val){
-//     console.log(val.textContent);
-// });
+console.log("JS Loaded");
 
-// let img = document.querySelector("img");
-// img.setAttribute("src","https://lh3.googleusercontent.com/gps-cs-s/APNQkAGmYE6clA17N-O09V1Ebptawxm-_peZWUGNHp9ca2eRoPJHBCVtRYkwHp3CNLHL1kawucVylGAPS_XBM5sqXSX8kOQsrgK_vHH_aCuhlygvX8iivCCYmh2DH52Y6_51r8KBj7Fm=s1360-w1360-h1020-rw");
-
-// let a = document.querySelector("a");
-// a.href = "https://www.sheryians.com";
-
-// let div = document.querySelector("div");
-// div.setAttribute("title","Some Info");
-
-// let button = document.querySelector("button");
-// button.removeAttribute("disabled");
-
-
-// let ul = document.querySelector("ul");
-// let li = document.createElement("li");
-// li.textContent = "Mango";
-// ul.appendChild(li);
-
-
-// let p = document.querySelector("p");
-// p.addEventListener("click",function(){
-//     p.classList.add("para");
-// });
-
-// p.addEventListener("dblclick",function(){
-//     p.style.color = "green";
-// });
-
-
+let inp = document.querySelector("input");
+console.log(inp);
+inp.addEventListener("input", function (d) {
+    console.log(d.data);
+});
